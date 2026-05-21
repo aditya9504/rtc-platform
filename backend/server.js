512e2e5d2e00e14b4f6084aafc41a734758b232a
aditya9504 <adityasoni0954@gmail.com>
2026-05-21 12:29:45 +0530

    prepared backend for render

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: "https://rtc-platform.vercel.app",
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
